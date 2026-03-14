3a9964c4a32511b58a2d4d8fe0a15389ff7363b8
tyr7z <tyr7z@users.noreply.github.com>
2026-03-14 10:48:35 +0100

    add one prettier-ignore comment

--- src/proto.ts
+++ src/proto.ts
@@ -4,6 +4,6 @@ import { join } from "path";
 const root = await load([
   join(import.meta.dir, "../proto/maestro_user.proto"),
   join(import.meta.dir, "../proto/client_server.proto"),
-]);
+]); // prettier-ignore
 
 export const Envelope = root.lookupType("maestro.user_proto.envelope");
